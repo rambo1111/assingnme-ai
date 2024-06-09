7a8efc973090fc33a0e3b52e05dd526430fd8026
rambo1111 <132392947+rambo1111@users.noreply.github.com>
2024-06-09 10:59:21 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,16 +1,23 @@
 from flask import Flask
-import request
+import requests
 import time
+import threading
 
 app = Flask(__name__)
 
+def continuous_requests():
+    while True:
+        response = requests.get("https://test-assingnement-api.onrender.com/keep-alive")
+        print(response.text)
+        time.sleep(2)
+
 @app.route('/keep-alive')
 def keep_alive():
-    while True:
-    response = requests.get("https://test-assingnement-api.onrender.com/keep-alive")
-    print(response.text)
-    time.sleep(2)
     return 'Service is alive!'
 
 if __name__ == '__main__':
+    # Start the continuous requests in a separate thread
+    threading.Thread(target=continuous_requests, daemon=True).start()
+    # Run the Flask app
     app.run(debug=True, port=8080)
+
