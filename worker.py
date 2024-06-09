12b0e7ec3be063ce159b5b2b72ba922db5f8dd14
rambo1111 <132392947+rambo1111@users.noreply.github.com>
2024-06-09 10:56:34 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import request
+import time
 
 app = Flask(__name__)
 
 @app.route('/keep-alive')
 def keep_alive():
+    while True:
+    response = requests.get("https://test-assingnement-api.onrender.com/keep-alive")
+    print(response.text)
+    time.sleep(2)
     return 'Service is alive!'
 
 if __name__ == '__main__':
