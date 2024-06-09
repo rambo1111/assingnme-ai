243fe161207146b9dca21bc168d470ea813bd666
rambo1111 <132392947+rambo1111@users.noreply.github.com>
2024-06-09 11:25:47 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 import requests
 import time
+import threading
 
 app = Flask(__name__)
 CORS(app)
@@ -13,16 +14,14 @@ def continuous_requests():
             print(response.text)
         except Exception as e:
             print(f"Error occurred: {e}")
-        time.sleep(2)
+        time.sleep(5)
 
 @app.route('/')
 def home():
-
-    continuous_requests()
-    
     return 'Welcome to the Keep-Alive Service!'
 
-
 if __name__ == '__main__':
+    # Start the continuous requests in a separate thread
+    threading.Thread(target=continuous_requests, daemon=True).start()
     # Run the Flask app
     app.run(debug=True, host='0.0.0.0', port=8080)
